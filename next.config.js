959acc9ca8e3e67fdc03507a00e506d6b6b9ca82
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-11-17 22:49:05 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    // experimental: {
+    //     experimentalServerComponents: false // Server Component の機能を無効にする
+    //   }
+    // 
+    images :{
+        domains :[
+            "cdn2.thecatapi.com"
+        ],
+
+    },
+    // experimental: {
+    //     stream: false,
+    //   },
+
+    //   experimental: {
+    //         console: true,
+    //       },
+
+
+}
 
 module.exports = nextConfig
+
+
+
+// npm run dev --no-experimental-features
