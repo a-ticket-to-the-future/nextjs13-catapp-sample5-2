03c2b094d41a3d0649b4a115626141896abeef6e
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-11-20 14:09:31 +0900

    videoスペース準備OK。次はstorageに動画を保存して呼び出してみよう

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@
 import { GetServerSideProps, NextPage } from 'next';
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react';
+import VideoComponent from './VideoComponent/page';
 
 
 
@@ -62,13 +63,20 @@ const Home : NextPage<IndexPageProps>  = ({initialCatImageUrl}) => {
 
 
   return (
-    <div className=' w-screen h-screen'>
-    <div>Hello</div>
-    {/* <Image src="https://cdn2.thecatapi.com/images/3lc.jpg" alt='' width={300} height={300}/> */}
-    <Image src={catImageUrl} alt='' width={300} height={300}/>
-    <button className=' border-2 rounded-lg border-black hover:scale-105 active:scale-95' onClick={handleClick}>
-      クリック
-    </button>
+    <div className=' w-screen h-screen '>
+      <div className=' border-b-2 border-blue-600 '>
+        <div className=' ml-80 pl-60 mt-20'>
+          <div className=' text-4xl font-bold ml-24 mb-5 pl-3'>Hello</div>
+            {/* <Image src="https://cdn2.thecatapi.com/images/3lc.jpg" alt='' width={300} height={300}/> */}
+            <Image src={catImageUrl} alt='' width={300} height={300}/>
+            <button className=' border-2 rounded-lg border-black mt-5 ml-24 px-5 mb-20 hover:scale-105 active:scale-95' onClick={handleClick}>
+              クリック
+            </button>
+          </div>
+        </div>
+        <div className='ml-80 pl-60'>
+          <VideoComponent />
+        </div>
     </div>
   )
 }
